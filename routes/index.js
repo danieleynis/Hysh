01c9ca2a4b001c00c67c3fb7a9ea3253f38f44db
Rudd Johnson <rujo2@pdx.edu>
2017-08-04 20:50:40 -0700

    Further merge conflict issues

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,3 @@
-/**************************************************************** 
-  * Copyright (c) 2017 Rudd Johnson, Daniel Eynis, Justin Moore *
-  * This program is licensed under the "MIT License".           *
-  * Please see the file COPYING in the source                   *
-  * distribution of this software for license terms.            *
-/****************************************************************/
-
 var express = require('express');
 var router = express.Router();
 
