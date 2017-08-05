2e1eda930334c04dd56ff9a64f3e3eec906bc8b4
jstar1 <jstar1@users.noreply.github.com>
2017-08-04 19:48:46 -0700

    Adds licence to each file

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
+/**************************************************************** 
+  * Copyright (c) 2017 Rudd Johnson, Daniel Eynis, Justin Moore *
+  * This program is licensed under the "MIT License".           *
+  * Please see the file COPYING in the source                   *
+  * distribution of this software for license terms.            *
+/****************************************************************/
+
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
