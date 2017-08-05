283c86154ba7b8c86822b6d3b648140849ebd782
Rudd Johnson <rujo2@pdx.edu>
2017-08-04 18:59:20 -0700

    added rendering for photos and confirmation pages in index.js

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,7 @@ module.exports = function(passport){
     res.send("Main page");
   });
 
+/*Nunjucks rendering for registration page*/
   router.get('/register', function(req, res){
     res.render('register.njk');
   });
@@ -17,8 +18,19 @@ module.exports = function(passport){
     failureRedirect: '/register'
   }));
 
+/*Nunjucks rendering for login page*/
   router.get('/login', function(req, res){
     res.render('login.njk');
+  });
+
+/*Nunjucks rendering for photos page*/
+router.get('/photos', function(req, res){
+    res.render('photos.njk');
+  });
+
+/*Nunjucks rendering for confirmation page page*/
+router.get('/confirmation', function(req, res){
+    res.render('confirmation.njk');
   });
 
   router.post('/login', passport.authenticate('login', {
