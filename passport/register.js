6bf0606ba5d57162760e97b6c2b8321aa41de5a0
jstar1 <jstar1@users.noreply.github.com>
2017-08-04 19:48:46 -0700

    Adds licence to each file

--- passport/register.js
+++ passport/register.js
@@ -1,3 +1,10 @@
+/**************************************************************** 
+  * Copyright (c) 2017 Rudd Johnson, Daniel Eynis, Justin Moore *
+  * This program is licensed under the "MIT License".           *
+  * Please see the file COPYING in the source                   *
+  * distribution of this software for license terms.            *
+/****************************************************************/
+
 var LocalStrategy = require('passport-local').Strategy;
 var User = require('../models/user.js');
 var bCrypt = require('bcrypt-nodejs');
