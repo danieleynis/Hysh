248545eb11e43ffda83c2a5db806f55d62e7c52f
frank <rujo2@pdx.edu>
2017-08-17 21:19:28 -0700

    finished validation for login and registration

--- routes/index.js
+++ routes/index.js
@@ -25,27 +25,30 @@ module.exports = function(passport){
   
   /*post from registration form, validate and authenticate user input*/
   router.post('/register', function(req,res,next) {
-  	console.log("in function!");
 	req.check('email','Not a valid email address').isEmail();
 	req.check('password','password must be atleast 5 characters').isLength({min:5});
-	req.check('password','both passwords must match').equals(req.body.reenterpassword);
+	req.check('reenterpassword','both passwords must match').equals(req.body.password);
 	var errors = req.validationErrors();
+
 	if(errors)
 	{
-		console.log(errors);
-		JSON.stringify(errors)
-		for(var i=0; i<2; ++i){
-		console.log(errors[i].msg);
-		}
-		res.render('register.njk',errors);
+		console.log(errors);			
+		res.render('register.njk',{errors:errors});
 		return;					//do not proceed to authentication if invalid
 	}
-	next();						//pass to input to passport authentication
-	},
-		passport.authenticate('register', {
-		successRedirect: '/login',
-		failureRedirect: '/register'
-  	}));
+	//after validating form input, authenticate with passport
+	passport.authenticate('register', function(err,proceed,info){
+	if(err){
+		return next(err);
+	}
+	if(proceed){
+		return res.redirect('/login');	//if username is not present, route to login
+	}
+	if(!proceed){	
+		res.render('register.njk',{usernameStatus: "Username is not available"}); //if username is present, rerender with error msg
+	}
+	})(req,res,next);
+  	});
 
   /*Nunjucks rendering for login page*/
   router.get('/login', function(req, res){
@@ -63,10 +66,30 @@ module.exports = function(passport){
     res.render('confirmation.njk');
   });
 
-  router.post('/login', passport.authenticate('login', {
-    successRedirect: '/home',
-    failureRedirect: '/login'
-  }));
+/*Validation and authentication for login page*/
+  router.post('/login', function(req,res,next) {
+	req.check('password','password must be atleast 5 characters').isLength({min:5});
+	var errors = req.validationErrors();
+
+	if(errors)
+	{
+		console.log(errors);			
+		res.render('login.njk',{error:"Password must be at least 5 characters"});
+		return;					//do not proceed to authentication if invalid
+	}
+	//after validating form input, authenticate with passport
+	passport.authenticate('login', function(err,proceed,info){
+	if(err){
+		return next(err);
+	}
+	if(proceed){
+		return res.redirect('/home');	//if username is not present, route to home
+	}
+	if(!proceed){	
+		res.render('login.njk',{usernameStatus: "Incorrect username or password"}); //if username is present, rerender with error msg
+	}
+	})(req,res,next);
+ });
 
   router.get('/logout', function(req, res){
     req.logout();
